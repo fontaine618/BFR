10bf14e6aa3fb8145df66c93a616dccc59964298
fontaine618 <simfont@umich.edu>
2025-09-30 01:50:21 -0400

    test run

--- experiments/microbiome/job.sh
+++ experiments/microbiome/job.sh
@@ -12,7 +12,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16g
-#SBATCH --output=/home/%u/logs/%x-%j.log
+#SBATCH --output=/storage/home/spf5519/work/BFR/experiments/microbiome/logs/microbiome_%A_%a.out
 # The application(s) to execute along with its input arguments and options:
 module load python/3.11.2
 source /storage/home/spf5519/work/BFR/bfr/bin/activate
